f1b7e88cf10dfc66ec637c0a4c34be16ff6626bf
iamkrincy <krincy6819@gmail.com>
2025-01-24 17:30:50 +0530

    calc

--- PR-3(Calculator)/vite app/src/Components/Calculator.jsx
+++ PR-3(Calculator)/vite app/src/Components/Calculator.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import "./Calculator.css";
 
-const Calculator = () => {
-  const [input, setInput] = useState("");
+const Calculator = () => {//
+  const [input, setInput] = useState("");  
+  const [result, setResult] = useState(""); 
 
   const handleButtonClick = (value) => {
     if (value === "C") {
       setInput("");
+      setResult("");
     } else if (value === "=") {
       try {
-        setInput(eval(input).toString());
+        const evalResult = eval(input); 
+        setResult(evalResult.toString());
       } catch {
-        setInput("Error");
+        setResult("Error");
       }
     } else {
       setInput(input + value);
@@ -22,33 +25,28 @@ const Calculator = () => {
     <div>
       <h1>CALCULATOR</h1>
       <div className="counter">
-        <div className="display">{input || "0"}</div>
+        <div className="display">
+          <div className="entered">{input || "0"}</div>
+          <div className="calculated">{result}</div>
+        </div>
         <div className="calc">
-          <div className="flex1">
+          <div className="buttons">
             <button onClick={() => handleButtonClick("C")}>C</button>
             <button onClick={() => handleButtonClick("(")}>(</button>
             <button onClick={() => handleButtonClick(")")}>)</button>
             <button onClick={() => handleButtonClick("+")}>+</button>
-          </div>
-          <div className="flex2">
             <button onClick={() => handleButtonClick("7")}>7</button>
             <button onClick={() => handleButtonClick("8")}>8</button>
             <button onClick={() => handleButtonClick("9")}>9</button>
             <button onClick={() => handleButtonClick("-")}>-</button>
-          </div>
-          <div className="flex3">
             <button onClick={() => handleButtonClick("4")}>4</button>
             <button onClick={() => handleButtonClick("5")}>5</button>
             <button onClick={() => handleButtonClick("6")}>6</button>
             <button onClick={() => handleButtonClick("*")}>*</button>
-          </div>
-          <div className="flex4">
             <button onClick={() => handleButtonClick("1")}>1</button>
             <button onClick={() => handleButtonClick("2")}>2</button>
             <button onClick={() => handleButtonClick("3")}>3</button>
             <button onClick={() => handleButtonClick("/")}>/</button>
-          </div>
-          <div className="flex5">
             <button onClick={() => handleButtonClick("0")}>0</button>
             <button onClick={() => handleButtonClick("%")}>%</button>
             <button onClick={() => handleButtonClick(".")}>.</button>
@@ -61,3 +59,4 @@ const Calculator = () => {
 };
 
 export default Calculator;
+
